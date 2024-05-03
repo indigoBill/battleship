4b97a9227a6a4628645f28c6234afd375dc131a0
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-05-03 09:38:29 -0400

    Create placeShip function

---
+++ src/factories/gameboard.js
@@ -0,0 +1,38 @@
+export default function Gameboard(){
+    const board = [[],[],[],[],[],[],[],[],[],[]];
+
+    function placeShip(obj, coordinates){
+        //  HORIZONTAL PLACEMENT
+        const row = coordinates[0];
+        const column = coordinates[1];
+
+        //  POSSIBLE POSITIONS ONBOARD ARE BETWEEN 0 & 9, INCLUSIVE
+        if(!(row < 10 && row > -1)) return false;
+
+        if(!(column < 10 && column > -1)) return false;
+
+        if(obj.getLength() > 1){
+            const endColumn = column + (obj.getLength() - 1);
+
+            if(endColumn < 10){
+                for(let spacesToFill = column; spacesToFill <= endColumn; spacesToFill+=1){
+                    board[row][spacesToFill] = obj;
+                }
+
+                return [[row, column],[row, endColumn]];
+            }
+
+            return false;
+        }
+
+        board[row][column] = obj;
+
+        return [row, column];   
+    }
+
+    function receiveAttack(coordinates){
+        
+    }
+
+    return { placeShip };
+}
