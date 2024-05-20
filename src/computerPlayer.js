0f48e367d3de70682b406954f59e471d127e964e
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-05-20 11:45:05 -0400

    Add complexity to computer moves

--- src/computerPlayer.js
+++ src/computerPlayer.js
@@ -5,8 +5,11 @@
 
 import { updateBoxDisplay } from './game-ui';
 
+const POSSIBLE_PLAYS = [[1,0],[-1,0],[0,1],[0,-1]];
 const nextPossibleMoves = [];
-let prevShotSuccessful;
+const successfulCoordinates = [];
+let direction;
+let nextMoveLogical;
 let row;
 let column;
 
@@ -17,16 +20,91 @@ function getRandomMove(){
     return [row, column];
 }
 
-function getLogicalMove(){
-    const prevMoveCoordinates = [row, column];
+function validateCoordinates(xCoor, yCoor){
+    if(xCoor > -1 && xCoor < 10){
+        if(yCoor > -1 && yCoor < 10){
+            return [xCoor, yCoor];
+        }
+    }
 
-    // row+1
-    prevShotSuccessful = false;
-    
+    return false;
+}
+
+function loadAllPossibleMoves(){
+    POSSIBLE_PLAYS.forEach((move) => {
+        const newRow = row + move[0];
+        const newColumn = column + move[1];
+
+        const validCoor = validateCoordinates(newRow, newColumn);
+
+        if(validCoor) nextPossibleMoves.push(validCoor);
+    });
+}
+
+function getLogicalMove(){
+    if(direction) return direction;
+
+    if(successfulCoordinates.length === 2){
+        const firstXCoor = successfulCoordinates[0][0];
+        const firstYCoor = successfulCoordinates[0][1];
+        const secondXCoor = successfulCoordinates[1][0];
+        const secondYCoor = successfulCoordinates[1][1];
+
+        const xDirection = firstXCoor - secondXCoor;
+        const yDirection = firstYCoor - secondYCoor;
+
+        if(xDirection > 0 && !yDirection) direction = 'LEFT';
+        else if(xDirection < 0 && !yDirection) direction = 'RIGHT';
+
+        if(yDirection > 0 && !xDirection) direction = 'UP';
+        else if(yDirection < 0 && !xDirection) direction = 'DOWN'
+
+        nextPossibleMoves.length = 0;
+        successfulCoordinates.length = 0;
+
+        return direction;
+    }
+
+    if(nextPossibleMoves.length === 0) loadAllPossibleMoves();
+        
+    const coordinates = nextPossibleMoves.shift();
+
+    [row, column] = [coordinates[0], coordinates[1]];
+
+    return [row, column];  
 }
 
 export default function getComputerMove(uiBoard, board){
-    const coordinates = (prevShotSuccessful) ? getLogicalMove() : getRandomMove();
+    const coordinates = (nextMoveLogical) ? getLogicalMove() : getRandomMove();
+
+    if(coordinates === direction){
+        switch(direction){
+            case 'LEFT':
+                row+=1;
+                break;
+            case 'RIGHT':
+                row-=1;
+                break;
+            case 'UP':
+                column+=1;
+                break;
+            default:
+                column-=1;
+                break;
+        }
+        
+        const validCoor = validateCoordinates(row, column);
+
+        if(validCoor){
+            //  ISSUE SINCE I SET COORDINATES TO A STRING I CANT GET ARRAY VALUES
+            [coordinates[0], coordinates[1]] = [row, column];
+        }else{            
+            const nextMove = getRandomMove();
+
+            [coordinates[0], coordinates[1]] = [nextMove[0], nextMove[1]];
+            nextMoveLogical = false;
+        }
+    }
 
     const uiRow = uiBoard.querySelector(`[row = "${coordinates[0]}"]`);
     const uiBox = uiRow.querySelector(`[box = "${coordinates[1]}"]`);
@@ -37,9 +115,15 @@ export default function getComputerMove(uiBoard, board){
         board.receiveAttack([row,column]);
 
         if(board.getBoard()[row][column]){
-            prevShotSuccessful = true;
+            if(!nextMoveLogical) nextMoveLogical = true;
+            successfulCoordinates.push([row, column]);
             updateBoxDisplay(uiBox, true);
         }else{
+            if(direction){
+                nextMoveLogical = false;
+                direction = null;
+            }
+
             updateBoxDisplay(uiBox);
         }
     }
