4396b1a545e90536b4d5b4dafe9b4f7519c7a0b2
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-05-24 13:14:48 -0400

    Add functionality to modal

--- src/game-logic.js
+++ src/game-logic.js
@@ -1,30 +1,25 @@
 import Player from './factories/player';
 import Ship from './factories/ship';
 
-import { generateUiBoard, toggleBoardState, updateBoxDisplay, setActiveBoard, updateUiBoard, updateClassList } from './game-ui';
-import getComputerMove from './computerPlayer';
+import { generateUiBoard, toggleBoardState, updateBoxDisplay, setActiveBoard, updateUiBoard, updateClassList, createRotateBtn, createSelectOpponentModal, hideOpponentModal } from './game-ui';
+import { getComputerMove, generateRandomShipPlacement } from './computerPlayer';
 
 //  /////////////////////////////////////////////////////////////////////////
 
-const player1 = Player('player1');
-const player2 = Player();
-const board1 = player1.getBoard();
-const board2 = player2.getBoard();
+const allShips = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)];
+let computerOpponent;
 
-// board1.placeShip(Ship(5), [0,0]);
-board2.placeShip(Ship(2), [9,0]);
+// const player1 = Player('player1');
+// const player2 = Player();
+// const board1 = player1.getBoard();
+// const board2 = player2.getBoard();
 
-const uiBoard1 = generateUiBoard(board1.getBoard());
-const uiBoard2 = generateUiBoard(board2.getBoard());
+// createRotateBtn();
+
+// const uiBoard1 = generateUiBoard(board1.getBoard());
+// const uiBoard2 = generateUiBoard(board2.getBoard());
 
 //  ///////////////////////////////////////////////////////////////////////////////
-
-function getAttackCoordinates(e){
-    const row = Number(e.target.parentElement.getAttribute('row'));
-    const column = Number(e.target.getAttribute('box'));
-
-    return [row,column];
-}
 
 function getNextMove(){
     toggleBoardState(uiBoard2);
@@ -35,10 +30,18 @@ function getNextMove(){
 
             if(board1.checkStatusOfShips()) console.log('GAME OVER');
             else toggleBoardState(uiBoard2);
+
         }, 500);
     }else{
         //  IF PLAYER IS PERSON SHOW NEXT PLAYER SCREEN FOR 5 SEC THEN CHANGE DISPLAY TO SHOW OTHER PLAYER BOARD
     }
+}
+
+function getAttackCoordinates(e){
+    const row = Number(e.target.parentElement.getAttribute('row'));
+    const column = Number(e.target.getAttribute('box'));
+
+    return [row,column];
 }
 
 function addEventListenerToBoard(uiBoard, board){
@@ -53,7 +56,7 @@ function addEventListenerToBoard(uiBoard, board){
 
                 if(board.getBoard()[coordinates[0]][coordinates[1]]) updateBoxDisplay(e.target, true);
                 else updateBoxDisplay(e.target);
-
+                
                 if(board.checkStatusOfShips()) console.log('GAME OVER');
                 else getNextMove();
             }
@@ -61,8 +64,13 @@ function addEventListenerToBoard(uiBoard, board){
     });
 }
 
-const allShips = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)];
+// function addEventListenerToRotateBtn(){
+//     const rotateBtn = document.querySelector('.rotate-btn');
 
+//     rotateBtn.addEventListener('click', (e) => {
+        
+//     });
+// }
 
 function placeShipsOnBoard(uiBoard, board){
     const boxes = uiBoard.querySelectorAll('.box');
@@ -105,30 +113,54 @@ function placeShipsOnBoard(uiBoard, board){
             const row = Number(e.target.parentElement.getAttribute('row'));
             const column = Number(e.target.getAttribute('box'));
 
-            if(board.placeShip(allShips[shipIndex], [row, column])){
+            if(shipIndex < allShips.length && board.placeShip(allShips[shipIndex], [row, column])){
                 updateUiBoard(uiBoard, board);
                 shipIndex += 1;
-            };
-
+            }else{
+                box.removeEventListener('click', )
+            }
         });
     });
 }
 
-export default function startGame(){
+function startGame(){
+    const player1 = Player('player1');
+    const player2 = computerOpponent ? Player() : Player('player2');
+    const board1 = player1.getBoard();
+    const board2 = player2.getBoard();
+    
+    createRotateBtn();
+    
+    const uiBoard1 = generateUiBoard(board1.getBoard());
+    const uiBoard2 = generateUiBoard(board2.getBoard());
 
     placeShipsOnBoard(uiBoard1, board1);
 
     if(player2.getPlayerName() === 'Computer'){
-        addEventListenerToBoard(uiBoard2, board2);
         setActiveBoard(uiBoard2);
+        generateRandomShipPlacement(board2, allShips);
+        addEventListenerToBoard(uiBoard2, board2);
     }else{
         addEventListenerToBoard(uiBoard1, board1);
         addEventListenerToBoard(uiBoard2, board2);
     }
 }
 
-//  2 LENGTH (X1)
-//  3 LENGTH (X2)
-//  4 LENGTH (X1)
-//  5 LENGTH (X1)
+function addEventListenerToModal(){
+    const opponentBtns = document.querySelectorAll('.opponent-btn');
 
+    opponentBtns.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            if(e.target.textContent === 'COMPUTER') computerOpponent = true;
+            else computerOpponent = false;
+
+            hideOpponentModal();
+            startGame();
+        });
+    });
+}
+
+export default function chooseOpponent(){
+    createSelectOpponentModal();
+    addEventListenerToModal();
+}
